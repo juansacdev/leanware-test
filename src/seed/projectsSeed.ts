5c59892182119195ebd261ceb9644d21778ea48a
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-11-02 17:11:36 -0500

    [add] migrations in seed format

---
+++ src/seed/projectsSeed.ts
@@ -0,0 +1,27 @@
+export const projectsSeed = [
+	{
+		name: 'Landing agencia de modelaje',
+		startDate: new Date(2020, 2, 23),
+		endDate: new Date(2021, 1, 10),
+	},
+	{
+		name: 'Scraper inmobiliaria',
+		startDate: new Date(2021, 3, 10),
+		endDate: new Date(2021, 6, 20),
+	},
+	{
+		name: 'App finanzas',
+		startDate: new Date(2021, 5, 23),
+		endDate: new Date(2022, 1, 10),
+	},
+	{
+		name: 'Pentesting',
+		startDate: new Date(2021, 3, 10),
+		endDate: new Date(2021, 8, 20),
+	},
+	{
+		name: 'ETL laboratorio',
+		startDate: new Date(2021, 9, 23),
+		endDate: new Date(2022, 5, 10),
+	},
+]
---
+++ src/seed/roleSeed.ts
@@ -0,0 +1,11 @@
+export const roleSeed = [
+	{
+		type: 'admin',
+	},
+	{
+		type: 'client',
+	},
+	{
+		type: 'operative',
+	},
+]
